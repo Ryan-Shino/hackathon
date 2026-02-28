cd135f9db11ea76992d7b48fbe087388d4b83ece
Milan <mjg1g24@soton.ac.uk>
2026-02-28 13:35:16 +0000

    decode-coord function

--- backend/TEST.py
+++
@@ -1,3 +0,0 @@
-import map_functions
-
-print(map_functions.decode_coords(-1.3979965792919415, 50.937801853482114))
--- backend/map_functions.py
+++ backend/map_functions.py
@@ -1,35 +1,35 @@
 import requests
 def decode_coords(long, lat):
     url = f"https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/{long},{lat}.json"
-    types = ["park", "gym", "library"]
 
     params = {
-        "radius": 1000000000,
+        "radius": 5000,
         "layers": "poi_label",
-        "limit": 100,
-        "access_token": "pk.eyJ1IjoibWkxeG4iLCJhIjoiY21tNjlnM3JqMGV6bTJvcXlkc2ljaGgzaSJ9.Z_WUVz3luhRGJTkCegC2mA",
+        "limit": 50,
+        "access_token": "sk.eyJ1IjoibWkxeG4iLCJhIjoiY21tNjl0bjJ6MGI2ZDJxcXkycmg2bHJ3aSJ9.x9jypoSNjEyhvDzSx9iRrg",
     }
 
     r = requests.get(url, params=params, timeout=5)
     r.raise_for_status()
     data = r.json()
-    
-    filtered = []
+    classNames = ["food_and_drink", "arts_and_entertainment", "park_like", "sport_and_leisure", "store_like", "education"]
+    simplified_types = {
+        "food_and_drink": "Social",
+        "sport_and_leisure": "Exercise",
+        "education": "Learning",
+        "park_like": "Nature",
+        "arts_and_entertainment": "Social",
+        "store_like": "Social"
+    }
     for feature in data.get("features", []):
         className = feature["properties"].get("class")
         distance = feature["properties"].get("distance")
-        if className == "food_and_drink":
-            filtered.append((className, distance))
+        typeName = feature["properties"].get("type")
+        distance = feature["properties"]["tilequery"].get("distance")
+        if className in classNames and typeName != "University":
+            if distance < 50:
+                return simplified_types[className]
+            else:
+                return ""
 
-        # if types is None or maki_type in types:
-        #     filtered.append({
-        #         "name": feature["properties"].get("name"),
-        #         "type": maki_type,
-        #         "class": feature["properties"].get("class"),
-        #         "distance": feature["properties"].get("distance"),
-        #     })
-
-    return filtered
     
-    #pk.eyJ1IjoibWkxeG4iLCJhIjoiY21tNjlnM3JqMGV6bTJvcXlkc2ljaGgzaSJ9.Z_WUVz3luhRGJTkCegC2mA
-    #sk.eyJ1IjoibWkxeG4iLCJhIjoiY21tNjl0bjJ6MGI2ZDJxcXkycmg2bHJ3aSJ9.x9jypoSNjEyhvDzSx9iRrg
